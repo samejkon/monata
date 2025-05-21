4498872c73738b2d6596ba6c326d7d89745f7f8e
tms-manhnguyen <manh.nguyen@tomosia.com>
2025-05-21 17:30:56 +0700

    Init migration

---
+++ monata-laravel/database/migrations/2025_05_21_072457_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->date('guest_name');
+            $table->date('guest_phone');
+            $table->date('guest_id');
+            $table->decimal('total_price', 10, 2);
+            $table->tinyInteger('status')->default(1);
+            $table->integer('creator_id');
+            $table->integer('last_modifierId')->nullable();
+            $table->integer('deleter_id')->nullable();
+            $table->softDeletes('deletion_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
---
+++ monata-laravel/database/migrations/2025_05_21_072529_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->decimal('price', 10, 2);
+            $table->string('type', 50);
+            $table->tinyInteger('status')->default(1);
+            $table->integer('creator_id');
+            $table->integer('last_modifierId')->nullable();
+            $table->integer('deleter_id')->nullable();
+            $table->softDeletes('deletion_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
