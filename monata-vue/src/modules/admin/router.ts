754ee1563218e25f89a6357afd405374ea4b988f
tms-manhnguyen <manh.nguyen@tomosia.com>
2025-05-22 14:36:37 +0700

    Refactor vue structure

---
+++ monata-vue/src/modules/admin/router.ts
@@ -0,0 +1,8 @@
+import DashBoard from './views/DashBoard.vue'
+
+export default [
+  {
+    path: '/dashboard',
+    component: DashBoard,
+  },
+]
---
+++ monata-vue/src/modules/customer/router.ts
@@ -0,0 +1,8 @@
+import Index from './views/Index.vue'
+ 
+export default [
+  {
+    path: '/',
+    component: Index,
+  },
+]
