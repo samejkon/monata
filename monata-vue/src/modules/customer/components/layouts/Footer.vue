83cf90256a717fbdcb44762b0d8a6ada1b68b133
tms-manhnguyen <manh.nguyen@tomosia.com>
2025-05-22 18:09:27 +0700

    add customer template

---
+++ monata-vue/src/modules/customer/components/layouts/Footer.vue
@@ -0,0 +1,80 @@
+<template>
+  <footer class="footer">
+    <div class="footer_top">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-3 col-md-6 col-lg-3">
+            <div class="footer_widget">
+              <h3 class="footer_title">
+                address
+              </h3>
+              <p class="footer_text"> 200, Green road, Mongla, <br>
+                New Yor City USA</p>
+              <a href="#" class="line-button">Get Direction</a>
+            </div>
+          </div>
+          <div class="col-xl-3 col-md-6 col-lg-3">
+            <div class="footer_widget">
+              <h3 class="footer_title">
+                Reservation
+              </h3>
+              <p class="footer_text">+10 367 267 2678 <br>
+                reservation@montana.com</p>
+            </div>
+          </div>
+          <div class="col-xl-2 col-md-6 col-lg-2">
+            <div class="footer_widget">
+              <h3 class="footer_title">
+                Navigation
+              </h3>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Rooms</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xl-4 col-md-6 col-lg-4">
+            <div class="footer_widget">
+              <h3 class="footer_title">
+                Newsletter
+              </h3>
+              <form action="#" class="newsletter_form">
+                <input type="text" placeholder="Enter your mail">
+                <button type="submit">Sign Up</button>
+              </form>
+              <p class="newsletter_text">Subscribe newsletter to get updates</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copy-right_text">
+      <div class="container">
+        <div class="footer_border"></div>
+        <div class="row">
+          <div class="col-xl-8 col-md-7 col-lg-9">
+            <p class="copy_right">
+              Copyright &copy;All rights reserved | This template is made with <i class="fa fa-heart-o"
+                aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            </p>
+          </div>
+          <div class="col-xl-4 col-md-5 col-lg-3">
+            <div class="socail_links">
+              <ul>
+                <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+                <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+                <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
+                <li><a href="#"> <i class="fa fa-youtube-play"></i> </a></li>
+              </ul>
+              <div class="footer-book-btn mt-4">
+                <a href="#booking-form" class="popup-booking btn footer-btn">Đặt phòng ngay</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- monata-vue/src/modules/customer/views/Index.vue
+++ monata-vue/src/modules/customer/views/Index.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import '../assets/css/main.css'
+import Header from '../components/layouts/Header.vue'
+import Home from '../components/pages/Home.vue'
+import Footer from '../components/layouts/Footer.vue'
+import CheckAvailable from '../components/forms/CheckAvailable.vue'
 </script>
 
 <template>
+  <Header />
+
   <main>
-    <h1>Index</h1>
+    <Home />
   </main>
+
+  <Footer />
+
+  <CheckAvailable />
 </template>
